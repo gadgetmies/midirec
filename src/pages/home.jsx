3158a11ad61365712cbf48a0573408f1020fb4ce
Glitch (glitch-hello-react) <none>
2022-09-26 18:04:10 +0000

    😀🍈 Checkpoint ./src/pages/home.jsx:25330432/9161

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,6 +2,20 @@ import React, { useState, useEffect, useRef } from "react";
 import { Link } from "wouter";
 import midi from "../midi";
 
+const messageToText = message => `${message.timeStamp}: [${
+              message.data.length
+            } bytes]: ${Array.from(message.data)
+              .map((d) => "0x" + d.toString(16))
+              .join(" ")}\n`;
+
+
+const initialiseDevices = async (setDevices) => {
+  console.log("initialising");
+  const midiAccess = await midi.initialize();
+
+  setDevices({ inputs: midi.getInputs(), outputs: midi.getOutputs() });
+};
+
 export default function Home() {
   const [midiMessages, setMidiMessages] = useState([]);
   const [currentPosition, setCurrentPosition] = useState([0, 0, 0, 0, 0]);
@@ -9,13 +23,6 @@ export default function Home() {
   const [selectedInput, setSelectedInput] = useState();
   const [selectedOutput, setSelectedOutput] = useState();
   const [inputListener, setInputListener] = useState();
-
-  const initialiseDevices = async () => {
-    console.log("initialising");
-    const midiAccess = await midi.initialize();
-
-    setDevices({ inputs: midi.getInputs(), outputs: midi.getOutputs() });
-  };
 
   useEffect(() => {
     function onMIDIMessage(event) {
@@ -32,25 +39,26 @@ export default function Home() {
       console.log(str);
     }
 
-    initialiseDevices();
+    initialiseDevices(setDevices);
   }, []);
 
   const previousSelectedInput = useRef();
   useEffect(() => {
-    console.log("input selected");
     if (previousSelectedInput.current !== selectedInput) {
       delete previousSelectedInput.onmidimessage;
       previousSelectedInput.current = selectedInput;
     }
-    selectedInput.onmidimessage = (e) => {
-      setMidiMessages([e, ...midiMessages]);
-      if (event.data[0] === 0xf8) {
-        const [ticks, phrase, bar, beat] = currentPosition;
-        setCurrentPosition([ticks + 1, phrase, bar, beat]);
-      } else if (event.data[0] === 0xfc) {
-        setCurrentPosition([0, 0, 0, 0, 0]);
-      }
-    };
+    if (selectedInput) {
+      selectedInput.onmidimessage = (e) => {
+        setMidiMessages([{data: e, text: messageToText(e)}, ...midiMessages]);
+        if (event.data[0] === 0xf8) {
+          const [ticks] = currentPosition;
+          setCurrentPosition([ticks + 1, ticks >> 4, ticks >> 8, ticks >> 10]);
+        } else if (event.data[0] === 0xfc) {
+          setCurrentPosition([0, 0, 0, 0, 0]);
+        }
+      };
+    }
   }, [selectedInput, midiMessages]);
 
   return (
@@ -103,18 +111,13 @@ export default function Home() {
         <button onClick={() => initialiseDevices()}>Refresh devices</button>
       </div>
       <div>
-        Position: {currentPosition[1]}.{currentPosition[2]}.{currentPosition[3]} ({currentPosition[0]})
+        Position: {currentPosition[1]}.{currentPosition[2]}.{currentPosition[3]}{" "}
+        ({currentPosition[0]})
       </div>
       <div>
         <button onClick={() => setMidiMessages([])}>Clear messages</button>
         <pre className="midi-messages">
-          {midiMessages.map((message) => {
-            return `${message.timeStamp}: [${
-              message.data.length
-            } bytes]: ${Array.from(message.data)
-              .map((d) => "0x" + d.toString(16))
-              .join(" ")}\n`;
-          })}
+          {midiMessages.slice(0, 20).map(({text}) => text)}
         </pre>
       </div>
     </>
